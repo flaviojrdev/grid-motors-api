fc9f58553a8d25a7e5d5e64c0273a6d854cb5e11
Flavio Junior <d-garm@outlook.com>
2023-04-05 09:09:39 -0400

    test(car): add car listing test

---
+++ src/api/useCases/car/listAll/listAll.test.ts
@@ -0,0 +1,13 @@
+import supertest from 'supertest'
+import app from '../../../../app'
+
+const request = supertest(app)
+
+const routeString = '/api/v1/car'
+
+describe('Car listing', () => {
+  test('Should be able to list all cars', async () => {
+    const response = await request.get(routeString)
+    expect(response.status).toBe(200)
+  })
+})
