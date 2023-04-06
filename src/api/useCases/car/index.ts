a28cba35baa53385ed94785b085ad77e0cbbfcac
Flavio Junior <d-garm@outlook.com>
2023-04-06 16:37:08 -0400

    feat(car): add get by id function in the index

--- src/api/useCases/car/index.ts
+++ src/api/useCases/car/index.ts
@@ -1,13 +1,15 @@
 import { registerCar } from './register/register'
 import { listAllCars } from './listAll/listAll'
+import { listCarById } from './listById/listById'
 import { removeCarById } from './remove/remove'
 import { updateCarById } from './update/update'
 
 const carUseCases = {
   registerCar,
   listAllCars,
+  listCarById,
   removeCarById,
-  updateCarById
+  updateCarById,
 }
 
 export default carUseCases
