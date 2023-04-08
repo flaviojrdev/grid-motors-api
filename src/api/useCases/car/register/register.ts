15ab751d7342015e501068a8a57d2ae73b74477e
Flavio Junior <d-garm@outlook.com>
2023-04-08 15:25:04 -0400

    fix(test): fix car registration test

--- src/api/useCases/car/register/register.ts
+++ src/api/useCases/car/register/register.ts
@@ -1,5 +1,5 @@
 import Car from '@entities/car'
-import { Request, Response} from 'express'
+import { Request, Response } from 'express'
 import carValidationSchema from '@utils/validateCar'
 
 export const registerCar = async (req: Request, res: Response) => {
@@ -13,8 +13,14 @@ export const registerCar = async (req: Request, res: Response) => {
 
     const newCar = new Car(carData)
     const result = await newCar.save()
+
     const { _id, __v, ...car } = result.toObject()
-    res.status(201).json(car)
+    const formattedResponse = {
+      _id: _id,
+      ...car,
+    }
+
+    res.status(201).json(formattedResponse)
   } catch (err) {
     return res.status(500).send('Internal server error')
   }
