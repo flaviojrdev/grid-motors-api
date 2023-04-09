143b4e1a923a259e972fd9c99e57e6de730ce964
Flavio Junior <d-garm@outlook.com>
2023-04-09 00:04:24 -0400

    refactor(user): remove validations from the schema, add uf field

--- src/api/entities/user.ts
+++ src/api/entities/user.ts
@@ -10,40 +10,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator: (cpf: string) => {
-        const cpfRegex = /^([0-9]){3}\.([0-9]){3}\.([0-9]){3}-([0-9]){2}$/
-        return cpfRegex.test(cpf)
-      },
-      message: (props: any) => `${props.value} is not a valid CPF!`,
-    },
   },
   birth: {
     type: String,
     required: true,
-    validate: {
-      validator: (dateString: string) => {
-        const [day, month, year] = dateString.split('/')
-        const birthDate = new Date(Number(year), Number(month) - 1, Number(day))
-        const diff = Date.now() - birthDate.getTime()
-        const ageDate = new Date(diff)
-        const age = Math.abs(ageDate.getUTCFullYear() - 1970)
-        return age >= 18
-      },
-      message: (props: any) => `You must be at least 18 years old to register!`,
-    },
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator: (email: string) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        return emailRegex.test(email)
-      },
-      message: (props: any) => `${props.value} is not a valid email!`,
-    },
   },
   password: {
     type: String,
@@ -55,7 +30,7 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   qualified: {
-    type: Boolean,
+    type: String,
     required: true,
   },
   patio: {
@@ -71,6 +46,10 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   locality: {
+    type: String,
+    required: true,
+  },
+  uf: {
     type: String,
     required: true,
   },
