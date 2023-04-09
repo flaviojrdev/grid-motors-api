e158913b887ea23d65ecf5d6093ec9f4be38351f
Flavio Junior <d-garm@outlook.com>
2023-04-09 00:17:23 -0400

    refactor(reserve): update index imports

--- src/api/useCases/reserve/index.ts
+++ src/api/useCases/reserve/index.ts
@@ -2,12 +2,14 @@ import { registerReserve } from './register/register'
 import { listAllReserves } from './listAll/listAll'
 import { removeReserveById } from './remove/remove'
 import { updateReserveById } from './update/update'
+import { listReserveById } from './listById/listById'
 
 const reservationUseCases = {
   registerReserve,
   listAllReserves,
   removeReserveById,
-  updateReserveById  
+  updateReserveById,
+  listReserveById,
 }
 
 export default reservationUseCases
